88d1237ba9c0f42758b9419f9148006087b70f4c
Tiago Silva <tiagobsb84@gmail.com>
2023-08-19 18:50:46 -0300

    Utilizando middleware nas rotas, finalizando o projeto

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ app.use(express.json());
 
 const pedidos = [];
 
+//Adicionando middleware para auxiliar em códigos repetidos.
+const middlewareFindIndex = (request, response, next) => {
+    const {id} = request.params;
+    const checkId = pedidos.findIndex(use => use.id === id);
+    if(checkId < 0) {
+        return response.status(404).json({error: "Object not found"});
+    }
+
+    request.userId = id;
+    request.toCheckId = checkId;
+
+    next();
+}
+
+const middlewareFind = (request, response, next) => {
+    const {id} = request.params;
+
+    const checkId = pedidos.find(use => use.id === id);
+    if(!checkId) {
+        return response.status(404).json({error: 'Object not found!'});
+    }
+
+    request.userId = id;
+    request.toCheckId = checkId;
+    
+    next();
+}
+
 //Listando todos pedido
 app.get('/pedidos', (request, response) => {
     return response.json(pedidos);
@@ -24,15 +52,11 @@ app.post('/pedidos', (request, response) => {
 });
 
 //Atualizar pedido
-app.put('/pedidos/:id', (request, response) => {
-    const {id} = request.params;
+app.put('/pedidos/:id', middlewareFindIndex,(request, response) => {
+    const id = request.userId;
+    const checkId = request.toCheckId;
     const {order, clientName, price} = request.body;
     const updateBody = {id, order, clientName, price};
-
-    const checkId = pedidos.findIndex(use => use.id === id);
-    if(checkId < 0) {
-        return response.status(404).json({error: "Object not found"});
-    }
 
     pedidos[checkId] = updateBody;
 
@@ -40,13 +64,8 @@ app.put('/pedidos/:id', (request, response) => {
 });
 
 //Deletando pedido
-app.delete('/pedidos/:id', (request, response) => {
-    const {id} = request.params;
-
-    const checkId = pedidos.findIndex(use => use.id === id);
-    if(checkId < 0) {
-        return response.status(404).json({error: 'Object not found!'});
-    }
+app.delete('/pedidos/:id', middlewareFindIndex, (request, response) => {
+    const checkId = request.toCheckId;
 
     pedidos.splice(checkId, 1);
 
@@ -54,25 +73,17 @@ app.delete('/pedidos/:id', (request, response) => {
 });
 
 //Buscando apenas um pedido
-app.get('/pedidos/:id', (request, response) => {
-    const {id} = request.params;
-
-    const checkId = pedidos.find(use => use.id === id);
-    if(!checkId) {
-        return response.status(404).json({error: 'Object not found!'});
-    }
+app.get('/pedidos/:id', middlewareFind, (request, response) => {
+    const id = request.userId;
+    const checkId = request.toCheckId;
 
     return response.json(checkId);
 });
 
 //Alterando um atributo pelo id
-app.patch('/pedidos/:id', (request, response) => {
-    const {id} = request.params;
-
-    const checkId = pedidos.find(use => use.id === id);
-    if(!checkId) {
-        return response.status(404).json({error: 'Object not found!'});
-    }
+app.patch('/pedidos/:id', middlewareFind, (request, response) => {
+    const id = request.userId;
+    const checkId = request.toCheckId;
 
     checkId.status = 'pronto';
 
