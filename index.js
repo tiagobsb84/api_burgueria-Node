fc8d045278e97d0bf70fa71fb06c6a97a8111258
Tiago Silva <tiagobsb84@gmail.com>
2023-08-19 09:41:21 -0300

    rota post para criar pedidos

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ app.use(express.json());
 
 const pedidos = [];
 
+//Listando todos pedido
 app.get('/pedidos', (request, response) => {
     return response.json(pedidos);
 });
+
+//Criando pedido
+app.post('/pedidos', (request, response) => {
+    const {order, clientName, price} = request.body;
+
+    const pedido = {id: uuid.v4(), order, clientName, price, status: 'Em preparação'};
+
+    pedidos.push(pedido);
+
+    return response.status(201).json(pedido);
+})
 
 app.listen(port, () => {
     console.log(`Porta inicializada: ${port}`);
